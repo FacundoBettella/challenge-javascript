e7cde51a5b8ed07b1c660af66da9b2453bb2d86a
Facundo <facundobettella@outlook.com>
2021-06-30 08:54:25 -0300

    JSPractice

--- checkpoint.js
+++ checkpoint.js
@@ -15,26 +15,32 @@ const {
     BinarySearchTree
 } = require('./DS.js');
 
+
 // ----- Closures -----
 
 // EJERCICIO 1
 // Implementar la funcion 'exponencial' que recibe un parametro entero 'exp'
-// y retorna una una funcion, nos referiremos a esta ultima como funcion hija,
+// y retorna una funcion, nos referiremos a esta ultima como funcion hija,
 // y a 'exponencial' como la funcion padre, la funcion hija debe de recibir 
 // un parametro y retornar dicho parametro elevado al parametro 'exp' de 
 // la funcion padre original 'exponencial'
 // Ejemplo:
 // > var sqrt = exponencial(2);
-// > sqrt(2);
+// > exponencial(2);
 // < 4
 // > sqrt(3);
 // < 9
 // > sqrt(4);
 // < 16
+ 
+function exponencial(exp) {              
+    
+    return function hija(x) {
+        return  x**exp;
+    }
+}    
 
-function exponencial(exp) {
 
-}
 
 // ----- Recursión -----
 
@@ -44,18 +50,18 @@ function exponencial(exp) {
 // que se deben realizar, para llegar al destino de un laberinto dado.
 //
 // Ejemplo: dado el siguiente laberinto:
-// let laberintoExample = { // direccion = ""
+// let laberintoExample = {
 //     N: 'pared',
-//     S: { // direccion = "S"
+//     S: {
 //         N: 'pared',
 //         S: 'pared',
-//         E: { // direccion = "SE"
-//             N: 'destino', // direccion = "SEN"
+//         E: {
+//             N: 'destino', ==> SEN
 //             S: 'pared',
 //             E: 'pared',
 //             O: 'pared'
 //         },
-//         O: { // direccion = "SO"
+//         O: {
 //             N: 'pared',
 //             S: 'pared',
 //             E: 'pared',
@@ -67,11 +73,21 @@ function exponencial(exp) {
 // }
 // El retorno de la funcion 'direcciones' debe ser 'SEN', ya que el destino se encuentra
 // haciendo los movimientos SUR->ESTE->NORTE
-// Aclaraciones: el segundo parametro que recibe la funcion ('direccion') puede ser pasado vacio (null)
+// Aclaraciones: el segundo parametro que recibe la funcion r prop') puede ser pasado vacio (null)
+function direcciones(laberinto, camino ="") {
+    if(!laberinto) return "";
 
-function direcciones(laberinto) {
-
-}
+    for (let puntoCardinal in laberinto){  
+        if(typeof (laberinto[puntoCardinal]) === "object"){
+            camino += puntoCardinal;//suma la key;
+            return  camino + direcciones(laberinto[puntoCardinal]);//Y hace recursion desde ese value;
+        }
+        if(laberinto[puntoCardinal] === "destino"){//Si no hay un objeto, es porque llego al final;
+            return camino = camino + puntoCardinal;
+        }
+    };
+    return ""; //Laberinto sin destino retorna "".
+};
 
 
 // EJERCICIO 3
@@ -88,9 +104,35 @@ function direcciones(laberinto) {
 // deepEqualArrays([0,1,[[0,1,2],1,2]], [0,1,[[0,1,2],1,2]]) => true
 
 function deepEqualArrays(arr1, arr2) {
-
+//Debo return true o false si los arrays son iguales o no. 
+//1ro comparo el largo de ambos.
+//2do si son del mismo tipo typeOf, no  hace falta! porque..  == el doble igual compara valores (1 =="1"--> true ). 
+// El === tambien chequea que sean el mismo tipo de datos (1 === "1" => false). 
+//3ro Comparo que el atraves de un for si ambos indices son iguales.
+//4to Si son distintos
+//5to Si ambos son array. utilizo instanceof.(Comparo si son array ambos, si lo son hago la recursividad deepEqualArrays).
+// (Hay que utilizar una bandera, empieza en false. Pero por defecto es true en el argumento).
+    if(arr1.length != arr2.length) return false;    
+    let bandera = false;    
+    for(let i = 0; i < arr1.length; i++){//Necesito un solo "for" para compararlos..
+        if(typeof arr1[i] === 'object'){//Si en esta parte del arr1, tengo otro arr(o un obj)..
+            for(element in arr1[i]){//Comienzo a iterar sobre ese objeto-array "interno".
+                if(arr1[i][element] === arr2[i][element]){
+                    bandera = true;
+                }
+                else{
+                    bandera = false;//Retorna false, porque los subelementos no son iguales.
+                }    
+            }
+        }
+        else if(arr1[i] === arr2[i]){//Si no hay arrays anidados..
+            bandera = true;
+        }
+    };
+    return bandera;
 }
-
+// [0,1,[[0,1,2],1,2]], => [1, 2, [1, 2, [0, 1, 2]]]   =>Se comporta como objeto=> [1, 2, {0:1, 1:2, {0:0, 1:1, 2:2}}];  
+// [0,1,[[0,1,2],1,2]                 
 
 
 // ----- LinkedList -----
@@ -121,7 +163,6 @@ OrderedLinkedList.prototype.print = function(){
     return print
 }
 
-
 // EJERCICIO 4
 // Crea el metodo 'add' que debe agregar nodos a la OLL de forma que la misma se conserve ordenada:
 // Ejemplo:
@@ -136,13 +177,68 @@ OrderedLinkedList.prototype.print = function(){
 // < 'head --> 5 --> 1 --> null'
 // > LL.add(4)
 // > LL.print()
-// < 'head --> 5 --> 3 --> 1 --> null'
-//               4
-OrderedLinkedList.prototype.add = function(val){
-    
+// < 'head --> 5 --> 4 --> 1 --> null'
+//               
+ 
+OrderedLinkedList.prototype.add = function(value){
+    var node = new Node(value);
+    let current = this.head;
+
+    //Casos: Lista vacia, add al principio de la lista, en el medio, agregar al final?
+    if(!current){
+        this.head = node;
+        return;
+    }    
+    //Add al principio de la lista.
+    if(current.value < node.value){
+        let aux = current;
+        current = node;
+        node.next = aux;
+        return;   
+    };
+
+    while(current.next){
+        if(current.next.value > node.value){//El siguiente valor es mas grande que el node value => avanza.
+            current = current.next
+        }
+        if(node.value > current.next.value){//El siguiente valor es mas chico que el node value => introducite.
+            let aux = current.next;
+            current.next = node;
+            node.next = aux;
+            return
+        }
+    }
+    current.next = node;
+    return;      
 }
 
+// OrderedLinkedList.prototype.add = function (val) {
 
+//     current = this.head;
+//     node = new Node(val);
+
+//     if (!this.head) {
+//         this.head = node;
+//         //        this._length++;
+//         return node;
+//     }
+
+//     if (val > current.value) {
+//         node.next = current;
+//         this.head = node;
+//         //        this._length++;
+//         return node;
+//     }
+
+//     while (current.next && !(val < current.value && val > current.next.value)) {
+//         current = current.next
+//     }
+//     var aux = current.next
+//     current.next = node
+//     node.next = aux
+//     return node;
+// }
+    
 // EJERCICIO 5
 // Crea el metodo 'removeHigher' que deve devolver el valor mas alto de la linked list 
 // removiendo su nodo corresponidente:
@@ -159,9 +255,32 @@ OrderedLinkedList.prototype.add = function(val){
 // < null
 
 OrderedLinkedList.prototype.removeHigher = function(){
-    
+    let current = this.head;
+    if(!current){
+        return null;
+    }
+    //Funciona solo porque la lista está ordenada => H --> 5 --> 4 --> 1 --> null;
+    let val = current.value;
+    this.head = current.next;
+    return val;    
 }
-
+/*Metodo para saber cual es el mas grande (no los saca de la linked).
+let current = this.head;
+    if(!current){
+        return null;
+    }
+while(!!current.next){
+    container.push(current);
+    current = current.next;
+}
+let max = 0;
+for(let i = 0; i < container.length; i++){
+    if(container[i] > max){
+        max += container[i]
+    }
+}
+return max;
+*/
 
 // EJERCICIO 6
 // Crea el metodo 'removeLower' que deve devolver el valor mas bajo de la linked list 
@@ -178,11 +297,32 @@ OrderedLinkedList.prototype.removeHigher = function(){
 // > LL.removeHigher()
 // < null
 
+//MISMO CODIGO QUE REMOVE LAST - SOLO FUNCIONA CON UNA LISTA ORDENADA DE > a <.
 OrderedLinkedList.prototype.removeLower = function(){
-    
+    current = this.head; 
+    if(!current){
+         return null;
+    }
+    //Cuando hay solo un nodo en la lista.
+    if(current.next === null){ 
+        var aux = current.value;
+        this.head = null;
+        return aux;
+    }
+    while(current.next.next){
+        current = current.next;
+    }
+    aux = current.next.value;
+    current.next = null;
+    return aux;
 }
-
-
+//METODO PARA DEJAR EL PRIMERO Y EL ULTIMO EN UNA LISTA.
+/*  while(current.next.next){
+        aux = current.next;
+        current.next = null;
+        return aux;
+    }    
+*/         
 
 // ----- QUEUE -----
 
@@ -212,9 +352,23 @@ OrderedLinkedList.prototype.removeLower = function(){
 // < ["2-1", "1-1", "1-2", "2-2"];
 
 function multiCallbacks(cbs1, cbs2){
-    
-}
-
+    let arr = [];
+    for(element in cbs1){
+        for(element in cbs2){
+            if(typeof element === 'function'){            
+                setTimeout(function() { arr.push(element(cb)); }, element[time]);
+            }                      
+        }            
+    };
+    return arr;     
+} 
+// function printing() {
+//     console.log(1);
+//     setTimeout(function() { console.log(2); }, 1000);
+//     setTimeout(function() { console.log(3); }, 0);
+//     console.log(4);
+//  }
+ 
 
 
 // ----- BST -----
@@ -230,10 +384,20 @@ function multiCallbacks(cbs1, cbs2){
 // 5   9
 // resultado:[5,8,9,32,64]
 
-BinarySearchTree.prototype.toArray = function() {
-    
+BinarySearchTree.prototype.toArray = function(order, array=[]) {
+    if(order === 'in-order'|| !order){
+        if(this.left){
+            this.left.toArray(order, array);
+       }
+       if(this.value){
+           array.push(this.value);
+       }
+       if(this.right){
+           this.right.toArray(order, array);
+       }
+    }
+    return array;
 }
-
 
 
 // ----- Algoritmos -----
@@ -249,9 +413,19 @@ BinarySearchTree.prototype.toArray = function() {
 // Si bien esta no es la mejor implementacion existente, con que uds puedan 
 // informarse sobre algoritmos, leerlos de un pseudocodigo e implemnterlos alcanzara
 
-function primalityTest(n) {
-    
+function primalityTest(num) {
+    if (num <= 3) return num > 1;//returna 2 y 3 true;
+    if ((num % 2 === 0) || (num % 3 === 0)) return false;//El numero primo solo es divisible por si mismo y 1 (Ya descartado 2 y 3);
+    let numPrimo = true;//Seteamos en true;
+    for( let i = 4; i < num/2; i++){ //Hasta num/2 para optimizar el tiempo.
+        if(num % i === 0){
+            numPrimo = false;
+            break;//si es false, corta el for;
+        }        
+    }
+    return numPrimo;
 }
+
 
 
 // EJERCICIO 10
@@ -262,10 +436,6 @@ function primalityTest(n) {
 function quickSort(array) {
     
 }
-// QuickSort ya lo conocen solo que este 
-// ordena de mayor a menor
-// para esto hay que unir como right+mid+left o cambiar el 
-// signo menor en la comparacion con el pivot
 
 
 
@@ -283,7 +453,7 @@ function quickSort(array) {
 // < 32859
 
 function reverse(num){
-    
+   
 }
 // la grandiosa resolucion de Wilson!!!
 // declaran una variable donde 
